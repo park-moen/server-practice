714963e3a6d354add7ff8c2912482be81a9255a2
park-moen <pmj6516@naver.com>
2022-04-21 17:50:41 +0900

    Part1 and Part2

---
+++ routes/page.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.locals.user = null;
+  res.locals.follwerCount = 0;
+  res.locals.fallowingCount = 0;
+  res.locals.follwerIdList = [];
+
+  next();
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird' });
+});
+
+router.get('/join', (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
+});
+
+router.get('/', (req, res, next) => {
+  const twits = [];
+
+  res.render('main', {
+    title: 'NodeBird',
+    twits,
+  });
+});
+
+module.exports = router;
